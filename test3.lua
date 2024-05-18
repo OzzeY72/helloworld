759058551348b7369d16b0ee92efa2fadba05a70
OzzeY72 <nabacap72@gmail.com>
2024-05-18 17:35:00 +0200

    + mvoe

--- test3.lua
+++ test3.lua
@@ -3,3 +3,4 @@ local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 local backpack = Player:FindFirstChildOfClass("Backpack")
 print(backpack)
+Player:Move(Vector3.new(0,0,0),false)
